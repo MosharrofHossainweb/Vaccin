396957d3603752d9212e3972aa8909f7ceb54a13
Mosharrof Hossain <mosharrofhossainpdl@gmail.com>
2024-10-20 15:46:11 +0600

    first commit

--- src/Component/Result/Result.jsx
+++ src/Component/Result/Result.jsx
@@ -1,5 +1,7 @@
 import './Result.css'
 import { Link } from 'react-router-dom'
+import { FaArrowRight } from "react-icons/fa";
+
 const Result = () => {
   return (
     <>
@@ -17,9 +19,53 @@ const Result = () => {
                 </div>
             </div>
                 <h3>Need a certificate for your COVID-19 result? Please click here</h3>
+          
+          </div>
+          <div className="why">
+
+            <div className="why_head">
+              <h2>Why get vaccinated
+              today?</h2>
+              <p>Magna adipiscing at elit at ornare lectus nibh lorem. 
+Ac, sed ac lorem pellentesque vestibulum risus matti. 
+In molestie condimentum malesuada non.</p>
+            </div>
+
+            <div className="why_protec">
+              <div className="protect1">
+                <img src="images\Group 31.png" alt="" />
+                <h2>Protects your immune 
+                system against viruses</h2>
+                <p>Velit ut consectetur mauris, orci amet, 
+                      faucibus. 
+                      Sit turpis fringilla ipsum pretium,
+                      dictum. Dolor eget vel nulla lorem ac.</p>
+                  <h3>Read More<FaArrowRight />
+                  </h3>
+              </div>
+
+              <div className="Protec_part2  lg:mt-[200px] md:mt-[30px] mt-[20px] flex flex-wrap lg:gap-[70px] md:gap-[50px] gap-[30px]">
+                    <div className="protect2 hover:scale-[1.1]">
+                      <img src="images\Group 32.png" alt="" />
+                      <h2>Minimize the spread
+                      of the Virus</h2>
+
+                        <h3>Read More<FaArrowRight />
+                        </h3>
+                    </div>
+                    <div className="protect2 hover:scale-[1.1] ">
+                      <img src="images\Group 33.png" alt="" />
+                      <h2>Protect yourself</h2>
+
+                        <h3>Read More<FaArrowRight />
+                        </h3>
+                    </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
+      
     </>
   )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,11 @@
 
 import Banner from "../Component/Banner/Banner";
 import Brand from "../Component/Brand/brand";
+import Covid from "../Component/Covid/Covid";
+import Feedback from "../Component/Feedback/Feedback";
+import Footer from "../Component/Footer/Footer";
 import Navber from "../Component/Navber/Navber";
+import Registraion from "../Component/Registration/Registraion";
 import Result from "../Component/Result/Result";
 
 
@@ -14,6 +18,10 @@ const Home = () => {
     <Banner/>
     <Brand/>
     <Result/>
+    <Registraion/>
+    <Covid/>
+    <Feedback/>
+    <Footer/>
     </>
   );
 }
